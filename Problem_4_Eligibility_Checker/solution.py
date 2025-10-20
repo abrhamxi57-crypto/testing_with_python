881226003d86724e8d4cbe04978962c2cae3fa2a
abrhamxi57-crypto <abrhamxi57@gmail.com>
2025-10-20 06:34:13 -0700

    Update solution.py

--- Problem_4_Eligibility_Checker/solution.py
+++ Problem_4_Eligibility_Checker/solution.py
@@ -1,1 +1,11 @@
+#eligibility checker whether you are eligible to drive cars in Ethiopia or not
+age=int(input("enter your age:"))
+if age>=16:
+    print("you completed the minimum requirement to drive cars in Ethiopia ")
+driving_license = input("Do you have a driving license?:")
+print(driving_license)
+if driving_license=='yes':
+    print("you're eligible to drive cars in Ethiopia! ")
+elif driving_license== 'no':
+    print("you are not eligible to drive cars in Ethiopia ")
 # your code here
