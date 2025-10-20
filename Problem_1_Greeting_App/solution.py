7c6538e37ab0246ee2b96ecb9c1d87f29662b5d7
abrhamxi57-crypto <abrhamxi57@gmail.com>
2025-10-20 06:55:58 -0700

    Update solution.py

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,13 @@
-# your code here
+
+name = input("Enter your name: ")
+favorite_food = input("Enter your favorite food: ")
+current_year = int(input("Enter the current year: "))
+age = int(input("Enter your age: "))
+
+# Calculate birth year
+birth_year = current_year - age
+
+# Print a personalized greeting
+print("Hello",name , "It's great to meet you.")
+print("You love",favorite_food )
+print("You were born in ",birth_year)
