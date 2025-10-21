d0eda70139c265ea865ea8ea6dfa0ff41a7d52a8
abrhamxi57-crypto <abrhamxi57@gmail.com>
2025-10-20 23:41:57 -0700

    Update solution.py

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,9 @@
+# Ask the user to enter temperature in Fahrenheit
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Convert Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5 / 9
+
+# Display the result
+print("The temperature in Celsius is:",celsius)
 # your code here
